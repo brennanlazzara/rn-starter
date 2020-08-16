02855c3d670c857301a7ab6d024ddf2978bda7cc
Brennan Predmore <brennnann@gmail.com>
2020-08-16 18:48:07 -0500

    new styling init to boxscreen.js // flex and box styling

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>child 1</Text>
+      <Text style={styles.textStyle}>child 2</Text>
+      <Text style={styles.textStyle}>child 3</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: 'black',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        height: 200,
+    },
+    textStyle: {
+        borderWidth: 3,
+        borderColor: 'red'
+    }
+});
+
+export default BoxScreen;
